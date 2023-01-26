a5a18e4c90ee47bb272ae2fcd54a6cc684bdfe12
Marcin Kacperski <kacperski81@gmail.com>
2023-01-26 10:15:57 +0000

    first full working version

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const questions = [
     {
         type: 'list',
         name: 'projectLicence',
-        message: 'What kind of licence should your project have', //user should see (Use arrow keys)
+        message: 'What kind of licence should your project have',
         choices: ['MIT','APACHE 2.0','GPL 3.0','BSD 3','None']
     },
     {
@@ -46,12 +46,37 @@ const questions = [
         message: 'What does the user need to know about contributing to the repo?'
     }
 ];
-//after the questions output 'Generating README...' 1:12 on the recording to see full pictrue of output
+
+
+// display correct license badge
+function checkBadges(data) {
+    switch(data) {
+        case ('MIT') :
+            return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+            break;
+        case ('APACHE 2.0') :
+            return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+            break;
+        case ('GPL 3.0') :
+            return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+            break;
+        case ('BSD 3') :
+            return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+            break;
+        case ('None') :
+            return 'None'
+            break;
+        default :
+            return ''
+    }
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
+    const badge = checkBadges(data.projectLicence);
+    data.projectLicence = badge;
     console.log(data);
-    const text = generateMarkdown(data)
+    const text = generateMarkdown(data);
     console.log(text);
     fs.writeFile(fileName,text,(err) => {
         if(err) return console.log(err);
